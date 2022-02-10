b03b3edaee6e1154932cea88b18ea2bd5dd52f20
zhuolisam <zhuolisam0627@gmail.com>
2022-02-11 00:06:14 +0800

    initial commit

---
+++ image.js
@@ -0,0 +1,39 @@
+const { ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 55164b6b544244678a8fb7cf91f63a14");
+
+const handleApiCall = (req, res) => {
+    const {input} = req.body;
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: "a403429f2ddf4b49b307e318f00e528b",
+            inputs: [{ data: { image: { url: input } } }]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.code + response.status.description + "\n" + response.status.details);
+                return;
+            }
+
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+            res.json(response);
+        }
+    );
+}
+
+module.exports = {
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+const imagedetect = require('./image')
+
+const db = knex({
+  // Enter your own database information here based on what you created
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port: '5432',
+    user : 'postgres',
+    password : 'system',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+app.get('/', (req, res)=> {
+  res.send(db.users);
+})
+
+app.post('/signin', (req, res) => {
+  console.log('signin is triggered')
+  const {email,password} = req.body;
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(`${password}`, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => console.log(err))
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+})
+
+
+app.post('/imageurl', (req, res) => { imagedetect.handleApiCall(req, res)})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(400).json('unable to get entries')})
+})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
+
