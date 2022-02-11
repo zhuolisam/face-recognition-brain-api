e919020b1b99b5acbc89323bd25d662868817efc
zhuolisam <zhuolisam0627@gmail.com>
2022-02-11 16:55:12 +0800

    fix cors 3

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ const app = express();
 app.use(cors(corsOption));
 // app.use(express.urlencoded({extended: true}));
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.get('/', (req, res)=> {
   res.send('it is working!');
