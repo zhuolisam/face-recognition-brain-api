07daad3357b884ea414fa47ac0d7c897c9748e6e
zhuolisam <zhuolisam0627@gmail.com>
2022-02-11 16:56:08 +0800

    fix cors 4

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(cors(corsOption));
 // app.use(express.urlencoded({extended: true}));
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', 'https://smart-brain-face-recognition-v.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTION');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
